933d64aabdc1f204d1060d70f2f1b302afb1e6f1
PoluektovaDarya <123874713+PoluektovaDarya@users.noreply.github.com>
2025-04-03 12:32:16 +0300

    Create constants.java

---
+++ src/constants.java
@@ -0,0 +1,19 @@
+package sample;
+public class constants {
+    public static final String USER_TABLE = "users";
+
+    public static final String USERS_ID = "idusers";
+    public static final String USERS_FIRSTNAME = "firstname";
+    public static final String USERS_LASTNAME = "secondname";
+    public static final String USERS_LASTNAME = "name";
+    public static final String USERS_LOGIN = "login";
+    public static final String USERS_PASSWORD = "password";
+
+    public static final String MEDICINE_TABLE = "medicine";
+    public  static final String ID_MEDICNE = "idMedicine";
+    public  static final String NAME_MEDICINE = "name";
+    public  static final String  APPLICATION = "application";
+    public  static final String LEFT_DAY = "left_day";
+    public  static final String COUNT = "count";
+    public  static final String EXPERATION_DATE = " expiration_date";
+}
