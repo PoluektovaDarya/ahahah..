c46b8de8e20385b0b0cd02b43e5cc69d4e2607af
PoluektovaDarya <123874713+PoluektovaDarya@users.noreply.github.com>
2025-04-03 12:33:06 +0300

    Create module-info.java

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module sample {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+    requires mysql.connector.java;
+    requires java.desktop;
+
+    opens sample;
+}
+
